e9cbfd65c31a21cf8c8dded1a89255310f4de2e8
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-09-17 09:48:08 +0530

    python

---
+++ BASICS/four.py
@@ -0,0 +1,27 @@
+eid = 100
+print(type(eid))
+esal = 45000
+print(type(esal))
+ename = 'raju'
+print(type(ename))
+availa = True
+print(type(availa))
+c= 10+300j
+print(type(c))
+r = range(10)
+print(type(r))
+l1 =[1,2,3,4,5,6,7]
+print(type(l1))
+t1 = (1,2,3,4,5,6,7)
+print(type(t1))
+s1 = {1,2,3,4,5,6,7}
+print(type(s1))
+emp1 ={'id':101,'name':'rahul'}
+print(type(emp1))
+
+b = bytes(l1)
+print(type(b))
+ba = bytearray(l1)
+print(type(ba))
+fa = frozenset(s1)
+print(type(fa))
