7ed66abd5c440df4a62bc182dec123389eac3e2a
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-09-23 15:58:09 +0530

    python

---
+++ PART-3/OOP-1/five1.py
@@ -0,0 +1,14 @@
+class Account:
+    def open_Account(self):
+        print("Account opened successfully")
+    def deposite(self):
+        print("deposited successfully")
+    def withdrawl(self):
+        print("Amount  withdrawl")
+    def get_bal(self):
+        print("low balance") 
+a1 = Account()                    
+a1.open_Account()
+a1.deposite()
+a1.withdrawl()
+a1.get_bal()
---
+++ PART-3/OOP-1/six1.py
@@ -0,0 +1,6 @@
+class Account:
+    @staticmethod
+    def deposite(a,b):
+        print("amount deposited")
+a1 =Account()        
+a1.deposite(1000,5000)
