a85721358d012285f2774cc43295fea3b3e0bd3e
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-10-22 10:54:27 +0530

    python

---
+++ PART-3/OOP-5-4Pillers/poly.py
@@ -0,0 +1,15 @@
+class Employee:
+    def cal_tax(self):
+        print("Employee class - cal_tax")
+
+class user:
+    def cal_tax(self):
+        print("user class - cal_tax")
+
+class Account:
+    def cal_tax(self):
+        print("Account class - cal_tax")        
+
+e1 = Employee()
+u1 = user()
+a1 = Account()
---
+++ PART-3/OOP-5-4Pillers/polyEx.py
@@ -0,0 +1,9 @@
+from poly import *
+
+def exec(obj):
+    obj.cal_tax()
+
+#exec(10)  
+exec(Employee())  
+exec (user())
+exec(Account())
