280e0a740a1f7754697fb4cf48276244500abbd3
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-10-29 16:49:53 +0530

    python

--- FILE HANDLING/CSV/online/FH2/empwrite.py
+++ FILE HANDLING/CSV/online/FH2/empwrite.py
@@ -1,11 +1,10 @@
 import csv
+
 fp=open('user.csv','w',newline='')
 csv_obj=csv.writer(fp)
-
-csv_obj.writerow(['username','emailid','location'])
-
-csv_obj.writerow(['Rahul','Rahul@gmail.com','chennai'])
-csv_obj.writerow(['sonia','sonia@gmail.com','noida'])
-
+csv_obj.writerow(['name','ID', 'location'])
+csv_obj.writerow(['muni', '101', 'tpt'])
+csv_obj.writerow(['raju', '102', 'skht'])
+csv_obj.writerow(['raani', '103', 'chennai'])
 
 fp.close()
