921ab7808f5383b31b47802809430bc6d31a0ae1
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-11-07 15:39:34 +0530

    python

--- PART-4/MYSQL/read.py
+++ PART-4/MYSQL/read.py
@@ -1,16 +1,21 @@
+
 import mysql.connector
 mycursor = None
 
 try:
-    dbcon = mysql.connector.connect(host ="localhost",user = "root",password = "root",database ="8pm" )
-    mycursor =dbcon.cursor()
-    mycursor.execute('select * from employee')
-    empdata = mycursor.fetchall()
+    dbcon = mysql.connector.connect(host="localhost",user = "root",password="root",database="8pm")
+    
+    mycursor = dbcon.cursor()
+    mycursor.execute('select eid,ename from employee')
+   
+    empdata =mycursor.fetchall()
     for emp in empdata:
         print(emp)
-except mysql.connector.DatabaseError as err:
-    if err:
-        print("unable to connect database")    
+
+except mysql.connector.DatabaseError as error:
+    if error:
+        print(error)
 finally:
+   
     mycursor.close()
     dbcon.close()
